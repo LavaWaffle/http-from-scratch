06dc17f8391e439f97d9d708b0c94d75c6a8c5f5
Hazel <bjxobjxo@gmail.com>
2025-08-16 22:11:42 -0400

    ch1 - l5

--- main.go
+++ main.go
@@ -9,40 +9,56 @@ import (
 	"bytes"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+
+	go func() {
+		defer f.Close()
+		defer close(lines)
+
+		buffer := make([]byte, 8)
+		var lineBuilder strings.Builder
+		for {
+			n, err := f.Read(buffer)
+			
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal("Error opening file", err)
+			}
+			
+			data := buffer[:n]
+			for {
+				i := bytes.IndexByte(data, '\n')
+				if i == -1 {
+					lineBuilder.Write(data)
+					break
+				}
+				lineBuilder.Write(data[:i])
+
+				lines <- lineBuilder.String()
+
+				lineBuilder.Reset()
+				data = data[i+1:]
+			}
+		}
+
+		if lineBuilder.Len() > 0 {
+			lines <- lineBuilder.String()
+		}
+	}()
+
+	return lines
+}
+
 func main() {
     file, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("Error opening file:", err)
 	}
-	defer file.Close()
-
-	buffer := make([]byte, 8);
-	var currentLine strings.Builder
-	for {
-		n, err := file.Read(buffer)
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			log.Fatal("Error reading file:", err)
-		}
-
-		data := buffer[:n]
-		for {
-			i := bytes.IndexByte(data, '\n')
-			if i == -1 {
-				currentLine.Write(data)
-				break
-			}
-			currentLine.Write(data[:i])
-
-			fmt.Printf("read: %s\n", currentLine.String())
-			currentLine.Reset()
-			data = data[i+1:]
-		}
-	}
-
-	if currentLine.Len() > 0 {
-		fmt.Printf("read: %s\n", currentLine.String())
+	
+	for line := range getLinesChannel(file) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
