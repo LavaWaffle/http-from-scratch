76c4578f7410dfaa948fb2850df89be8c4b8ae46
Hazel <bjxobjxo@gmail.com>
2025-08-16 21:14:07 -0400

    ch1 - lesson 3

--- main.go
+++ main.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-    fmt.Println("I hope I get the job!")
+    file, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("Error opening file:", err)
+	}
+	defer file.Close()
+
+	buffer := make([]byte, 8);
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			if err.Error() == "EOF" {
+				break
+			}
+			log.Fatal("Error reading file:", err)
+		}
+		fmt.Printf("read: %s\n" ,string(buffer[:n]))
+	}
 }
